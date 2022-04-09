a088f8868aef0bf968d80d6eaf7a8527efff8b76
Divine-oyegun <divineoyeguname>
2022-04-09 16:18:54 -0700

    0task

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -9,8 +9,10 @@
 int main(void)
 {
 	int n;
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
+	if (n > 0)
 	{
 		printf("%d is positive\n", n);
 	}
@@ -20,7 +22,7 @@ int main(void)
 	}
 	else
 	{
-		printf("%d is negative\n", n);
-	}
-	retur (0);
+		printf("%d is negative\n", n);												
+	}	
+	return (0);
 }
