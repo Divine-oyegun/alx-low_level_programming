12fed1fe41ea7c39deb90bb39681f9d4b0579674
Divine-oyegun <divineoyeguname>
2022-04-14 01:47:31 -0700

    main fix

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * main - main
  * Return: 0
@@ -6,11 +7,11 @@
 
 int main(void)
 {
-int i;
-i - 0;
+	int i;
 	
-positive_or_negative(i);
+	i = 0;
+	positive_or_negative(i);
 
-return (0);
+	return (0);
 
 }
